934fe35599915b38981cd328af6f7656fb80fa0a
Leeo <leeo1124@163.com>
2017-04-01 17:16:49 +0800

    test

---
+++ src/com/leeo/main/config_default.py
@@ -0,0 +1,21 @@
+'''
+Created on 2016年8月12日
+
+@author: Leeo
+
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ src/com/leeo/main/config_override.py
@@ -0,0 +1,13 @@
+'''
+Created on 2016年8月12日
+
+@author: Leeo
+
+Override configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
